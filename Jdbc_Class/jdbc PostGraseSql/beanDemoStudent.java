ba4e8ae11f1bc763ddc136bd8c4905d587fce4fa
Serious03Black <prathaamesh2509@gmail.com>
2024-08-30 19:33:10 +0530

    lec 78 rows set in jdbc

---
+++ Jdbc_Class/jdbc PostGraseSql/beanDemoStudent.java
@@ -0,0 +1,16 @@
+
+public class beanDemoStudent implements java.io.Serializable{
+    //bean properties 
+    int no;
+    String name;
+    //write setXxx(-)andgetXxx() method
+    public void setNo(int no){
+        this.no=no;
+    }
+    public int getNo(){
+        return no;
+    }
+    public void setName(String n){
+
+    }
+}
